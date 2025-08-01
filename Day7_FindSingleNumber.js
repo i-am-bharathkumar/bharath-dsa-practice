92c144e644d02d3414576bb493323f122366ddcf
Bharath <myself.bharathkumar@gmail.com>
2025-08-01 21:48:30 +0530

    Day 7 Find single number in an array

---
+++ Day7_FindSingleNumber.js
@@ -0,0 +1,16 @@
+// Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+// Note:
+// Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+    let res =0
+    for (num of nums){
+         res^=num
+    }
+    return res
+};
